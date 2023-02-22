a9217160093d1c8234c6a9c9f5a35c31fe100e07
Khushbu <khushbu.ladani@gmail.com>
2023-02-22 07:38:54 +0530

    adding files

---
+++ JobChain_01.java
@@ -0,0 +1,8 @@
+public class JobChain_01 {
+
+	public static void main(String[] args) {
+		System.out.println("This is job chaining 01");
+		System.out.println("Welcome to Jenkins");
+	}
+
+}
---
+++ JobChain_02.java
@@ -0,0 +1,8 @@
+public class JobChain_02 {
+
+	public static void main(String[] args) {
+		System.out.println("This is Job Chaining 02");
+		System.out.println("Welcome to Jenkins");
+	}
+
+}
